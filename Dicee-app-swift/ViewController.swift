1bac50cb41e83cb6456e941e1537c55bb1328409
alfeupanzobena <alfeupanzobena28@gmail.com>
2024-12-09 16:48:52 +0100

    add assets and updates

--- Dicee-app-swift/ViewController.swift
+++ Dicee-app-swift/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageViewOne:UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [
+            UIImage(imageLiteralResourceName: "DiceOne"),
+            UIImage(imageLiteralResourceName: "DiceTwo"),
+            UIImage(imageLiteralResourceName: "DiceThree"),
+            UIImage(imageLiteralResourceName: "DiceFour"),
+            UIImage(imageLiteralResourceName: "DiceFive"),
+            UIImage(imageLiteralResourceName: "DiceSix")
+        ]
+                
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+                
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
 
 }
 
